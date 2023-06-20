ff2389b5f1c0a21430f7c3b2f15676abdf7b1a63
Adotal <adotal1484@gmail.com>
2023-06-20 10:07:10 -0600

    Tetris.cpp updated

--- Tetris.cpp
+++ Tetris.cpp
@@ -2,10 +2,28 @@
 
 #include<iostream>
 #include<windows.h>
+using namespace std;
 
+void gotoXY(int x, int y) {
+    COORD pos;
+    pos.X = x;
+    pos.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
 
 int main(){
 
-	std::cout << "Tetris";
+	int i, j, x, y;
+	
+	int tetrominos[7];	// List of all pieces
+	for(i = 0; i < 7; ++i) tetrominos[i] = i+1;
+
+
+
+	do{
+
+
+	} while (!0);
+
 	return 0;
 }
